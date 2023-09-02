b6aa0b506da52b6171fd6586c9c2a2be42ad842c
JeongHunHui <deca3248@gmail.com>
2023-09-03 00:28:37 +0900

    feat: user 엔티티 구현 완료

---
+++ backend/src/main/java/com/hunhui/yeogiseonolja/domain/user/entity/User.java
@@ -0,0 +1,39 @@
+package com.hunhui.yeogiseonolja.domain.user.entity;
+
+import com.hunhui.yeogiseonolja.global.entity.BaseEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "users")
+public class User extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "phone", nullable = false, unique = true)
+    private String phone;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Builder
+    public User(String name, String phone, String email, String password) {
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.password = password;
+    }
+}
