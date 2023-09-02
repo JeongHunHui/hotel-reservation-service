ecc5d8e2db585230d39e097c18330c11ea48764a
JeongHunHui <deca3248@gmail.com>
2023-09-03 00:28:12 +0900

    feat: baseEntity 구현 완료

---
+++ backend/src/main/java/com/hunhui/yeogiseonolja/global/config/JpaAuditingConfig.java
@@ -0,0 +1,8 @@
+package com.hunhui.yeogiseonolja.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaAuditingConfig {}
---
+++ backend/src/main/java/com/hunhui/yeogiseonolja/global/entity/BaseEntity.java
@@ -0,0 +1,26 @@
+package com.hunhui.yeogiseonolja.global.entity;
+
+import java.time.LocalDateTime;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+@ToString
+public class BaseEntity {
+    @CreatedDate private LocalDateTime createdAt;
+
+    @LastModifiedDate private LocalDateTime updatedAt;
+
+    private boolean isDeleted;
+
+    public void deleteEntity() {
+        this.isDeleted = true;
+    }
+}
