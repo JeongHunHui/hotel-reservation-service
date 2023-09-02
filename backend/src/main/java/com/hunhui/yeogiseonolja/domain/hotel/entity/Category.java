244e2807a5239483e19e9b83dc3a357d397d06a0
JeongHunHui <deca3248@gmail.com>
2023-09-03 00:26:16 +0900

    feat: category 엔티티 구현 완료

---
+++ backend/src/main/java/com/hunhui/yeogiseonolja/domain/hotel/entity/Category.java
@@ -0,0 +1,26 @@
+package com.hunhui.yeogiseonolja.domain.hotel.entity;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Builder
+    public Category(String name) {
+        this.name = name;
+    }
+}
