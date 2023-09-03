07f4ef6d6618e63c7764b48c24fdd9423420f849
JeongHunHui <deca3248@gmail.com>
2023-09-04 03:37:47 +0900

    feat: 호텔 검색 service 부분 구현 완료

--- backend/src/main/java/com/hunhui/yeogiseonolja/domain/hotel/service/HotelService.java
+++ backend/src/main/java/com/hunhui/yeogiseonolja/domain/hotel/service/HotelService.java
@@ -1,6 +1,8 @@
 package com.hunhui.yeogiseonolja.domain.hotel.service;
 
 import com.hunhui.yeogiseonolja.domain.hotel.dto.HotelResponse;
+import com.hunhui.yeogiseonolja.domain.hotel.dto.HotelSearchRequest;
+import com.hunhui.yeogiseonolja.domain.hotel.dto.HotelSearchResponse;
 import com.hunhui.yeogiseonolja.domain.hotel.entity.Hotel;
 import com.hunhui.yeogiseonolja.domain.hotel.exception.HotelNotFoundException;
 import com.hunhui.yeogiseonolja.domain.hotel.mapper.HotelMapper;
@@ -9,14 +11,33 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public class HotelService {
-    private final HotelRepository hotelRepository;
-    private final HotelMapper hotelMapper;
+  private final HotelRepository hotelRepository;
+  private final HotelMapper hotelMapper;
 
-    public HotelResponse getHotelById(Long id) {
-        Hotel hotel = hotelRepository.findById(id).orElseThrow(HotelNotFoundException::new);
-        return hotelMapper.toDto(hotel);
-    }
+  public HotelResponse getHotelById(Long id) {
+    Hotel hotel = hotelRepository.findById(id).orElseThrow(HotelNotFoundException::new);
+    return hotelMapper.toDto(hotel);
+  }
+
+  public List<HotelSearchResponse> searchHotel(HotelSearchRequest searchRequest) {
+    List<HotelSearchResponse> hotels =
+        hotelRepository.findHotelsByCriteria(
+            searchRequest.getStartDate(),
+            searchRequest.getEndDate(),
+            searchRequest.getDetailRegionId(),
+            searchRequest.getCategoryId(),
+            searchRequest.getMinPrice(),
+            searchRequest.getMaxPrice(),
+            searchRequest.getMaxPeopleCount(),
+            searchRequest.getLimit(),
+            searchRequest.getOffset(),
+            searchRequest.getSortType());
+
+    return hotels;
+  }
 }
