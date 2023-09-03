a0d0751b8b7ce1b58764ae8abd7b805f1c9c81df
JeongHunHui <deca3248@gmail.com>
2023-09-04 03:38:16 +0900

    feat: 호텔 검색 API 구현 완료 및 응답코드 추가

--- backend/src/main/java/com/hunhui/yeogiseonolja/domain/hotel/controller/HotelController.java
+++ backend/src/main/java/com/hunhui/yeogiseonolja/domain/hotel/controller/HotelController.java
@@ -1,6 +1,8 @@
 package com.hunhui.yeogiseonolja.domain.hotel.controller;
 
 import com.hunhui.yeogiseonolja.domain.hotel.dto.HotelResponse;
+import com.hunhui.yeogiseonolja.domain.hotel.dto.HotelSearchRequest;
+import com.hunhui.yeogiseonolja.domain.hotel.dto.HotelSearchResponse;
 import com.hunhui.yeogiseonolja.domain.hotel.service.HotelService;
 import com.hunhui.yeogiseonolja.global.result.ResultCode;
 import com.hunhui.yeogiseonolja.global.result.ResultResponse;
@@ -8,6 +10,8 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/hotels")
@@ -19,5 +23,11 @@ public class HotelController {
     public ResponseEntity<ResultResponse> findHotelById(@PathVariable Long id) {
         HotelResponse hotelResponse = hotelService.getHotelById(id);
         return ResponseEntity.ok(ResultResponse.of(ResultCode.HOTEL_FIND_SUCCESS, hotelResponse));
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<ResultResponse> searchHotel(@ModelAttribute HotelSearchRequest searchRequest) {
+        List<HotelSearchResponse> hotelSearchResponses = hotelService.searchHotel(searchRequest);
+        return ResponseEntity.ok(ResultResponse.of(ResultCode.HOTEL_SEARCH_SUCCESS, hotelSearchResponses));
     }
 }
--- backend/src/main/java/com/hunhui/yeogiseonolja/global/result/ResultCode.java
+++ backend/src/main/java/com/hunhui/yeogiseonolja/global/result/ResultCode.java
@@ -9,7 +9,9 @@ public enum ResultCode {
 
     // 도메인 별로 나눠서 관리
     // hotel
-    HOTEL_FIND_SUCCESS("H00001", "호텔 조회 성공");
+    HOTEL_FIND_SUCCESS("H00001", "호텔 조회 성공"),
+    HOTEL_SEARCH_SUCCESS("H00002", "호텔 검색 성공"),
+    ;
 
     private final String code;
     private final String message;
